8e9ed7cdf9f06a74b222bc1c04cbbc6cd153eb47
samrawit <samri.berih@gmail.com>
2025-01-01 15:30:18 -0800

    fixed database config

--- backend/config/database.js
+++ backend/config/database.js
@@ -9,12 +9,27 @@ module.exports = {
     host: "127.0.0.1",
     dialect: "mysql",
   },
-  production: {
-    // username: process.env.DB_USERNAME,
-    // password: process.env.DB_PASSWORD,
-    // database: process.env.DB_DATABASE,
-    // host: process.env.DB_HOST,
-    use_env_variable: "DATABASE_URL",
-    dialect: "mysql",
+//   production: {
+//     // username: process.env.DB_USERNAME,
+//     // password: process.env.DB_PASSWORD,
+//     // database: process.env.DB_DATABASE,
+//     // host: process.env.DB_HOST,
+//     use_env_variable: "DATABASE_URL",
+//     dialect: "mysql",
+//   },
+production: {
+  use_env_variable: 'DATABASE_URL',
+  dialect: 'postgres',
+  seederStorage: 'sequelize',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
   },
+  define: {
+    schema: process.env.SCHEMA
+  }
+}
+
 };
