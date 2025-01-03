4271680c9c19ace3d94d57a4649f01bc7035ebe2
samrawit <samri.berih@gmail.com>
2025-01-02 20:07:35 -0800

    update

--- backend/db/seeders/20240923150843-Reviews.js
+++ backend/db/seeders/20240923150843-Reviews.js
@@ -210,7 +210,7 @@ module.exports = {
     options.tableName = 'Reviews';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(
-      "Reviews",
+      "Reviews", // if there is a problem with reviews down replace this with option.
       {
         review: {
           [Op.in]: [
