05e13aa4cb6ea4dec395ad2afd3bb0049b4191d7
samrawit <samri.berih@gmail.com>
2025-01-15 10:32:53 -0800

    uncomment bookings

--- backend/db/migrations/20240920213250-create-booking.js
+++ backend/db/migrations/20240920213250-create-booking.js
@@ -8,51 +8,51 @@ if (process.env.NODE_ENV === 'production') {
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-  //   await queryInterface.createTable('Bookings', {
-  //     id: {
-  //       allowNull: false,
-  //       autoIncrement: true,
-  //       primaryKey: true,
-  //       type: Sequelize.INTEGER
-  //     },
-  //     spotId: {
-  //       type: Sequelize.INTEGER,
-  //       allowNull: false,
-  //       references: {
-  //         model: 'Spots'
-  //       },
-  //       onDelete: 'CASCADE',
-  //     },
-  //     userId: {
-  //       type: Sequelize.INTEGER,
-  //       allowNull: false,
-  //       references: {
-  //         model: 'Users'
-  //       },
-  //       onDelete: 'CASCADE',
-  //     },
-  //     startDate: {
-  //       type: Sequelize.DATE
-  //     },
-  //     endDate: {
-  //       type: Sequelize.DATE
-  //     },
-  //     createdAt: {
-  //       allowNull: false,
-  //       type: Sequelize.DATE,
-  //       defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-  //     },
-  //     updatedAt: {
-  //       allowNull: false,
-  //       type: Sequelize.DATE,
-  //       defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-  //     }
-  //   }, 
-  //   options
-  // );
+    await queryInterface.createTable('Bookings', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      spotId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Spots'
+        },
+        onDelete: 'CASCADE',
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users'
+        },
+        onDelete: 'CASCADE',
+      },
+      startDate: {
+        type: Sequelize.DATE
+      },
+      endDate: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    }, 
+    options
+  );
   },
   async down(queryInterface, Sequelize) {
-   // options.tableName = 'Bookings';
-    //return queryInterface.dropTable(options);
+    options.tableName = 'Bookings';
+    return queryInterface.dropTable(options);
   }
 };
