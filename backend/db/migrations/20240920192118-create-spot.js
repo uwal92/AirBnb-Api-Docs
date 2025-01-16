781826c9ad3590a9158bbf2059470e58815a9eb3
samrawit <samri.berih@gmail.com>
2025-01-15 17:27:18 -0800

    changed project name

--- backend/db/migrations/20240920192118-create-spot.js
+++ backend/db/migrations/20240920192118-create-spot.js
@@ -75,7 +75,7 @@ module.exports = {
   }, 
   async down(queryInterface, Sequelize) {
     options.tableName = 'Spots';
-    await queryInterface.removeConstraint("AirBnb-Api-Docs.Bookings", "Bookings_spotId_fkey")
+    await queryInterface.removeConstraint("app_academy_project.Bookings", "Bookings_spotId_fkey")
     return queryInterface.dropTable(options);
   }
 };
