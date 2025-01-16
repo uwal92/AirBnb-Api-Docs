0c542b9de429a2534b0268ef5d63db1e64a400fb
samrawit <samri.berih@gmail.com>
2025-01-15 17:51:59 -0800

    added await

--- backend/db/migrations/20240920213250-create-booking.js
+++ backend/db/migrations/20240920213250-create-booking.js
@@ -53,6 +53,6 @@ module.exports = {
   },
   async down(queryInterface, Sequelize) {
     options.tableName = 'Bookings';
-    return queryInterface.dropTable(options);
+    return await queryInterface.dropTable(options);
   }
 };
