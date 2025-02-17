c9769461db0fe5a6744a4e33532acff446d3e398
samri berih <samri.berih@gmail.com>
2025-02-17 23:54:48 +0500

    Update auth.js

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -34,7 +34,7 @@ const setTokenCookie = (res, user) => {
   // Set the token cookie
   res.cookie("token", token, {
     maxAge: expiresIn * 1000, // maxAge in milliseconds
-    httpOnly: false,
+    httpOnly: true,
     secure: isProduction,
     sameSite: "None"//isProduction && "Lax",
   });
