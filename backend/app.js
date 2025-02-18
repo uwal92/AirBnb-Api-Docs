dc94874a4cc723681606108ade93fb57c497e53a
samri berih <samri.berih@gmail.com>
2025-02-18 05:12:35 +0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -52,7 +52,7 @@ app.use(express.json());
 //   app.use(cors());
 // }
 app.use(cors({
-  origin: ["https://airbnb-fe-8288.onrender.com"], 
+  origin: ["https://airbnb-fe-8288.onrender.com", "http://localhost:5173"], 
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true
 }));
